66a65429c8e21147030bfb479341cd24ff7c4d46
alexandremajda <majda.a@outlook.com>
2021-02-28 17:12:11 +0100

    [Feature] Adding HttpStatusCode for each route

--- src/main/java/com/quest/etna/controller/DefaultController.java
+++ src/main/java/com/quest/etna/controller/DefaultController.java
@@ -1,23 +1,28 @@
 package com.quest.etna.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DefaultController {
 
     @GetMapping(value = "testSuccess")
+    @ResponseStatus(code = HttpStatus.OK)
     public String testSuccess() {
         return "success";
     }
 
     @GetMapping(value = "/testNotFound")
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
     public String testNotFound() {
         return "not found";
     }
 
     @GetMapping(value = "/testError")
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
     public String testError() {
         return "error";
     }
