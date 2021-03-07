bfef6f83bd548ab89e20c228986f78adf29e03c2
alexandremajda <majda.a@outlook.com>
2021-03-07 23:21:07 +0100

    [Fix] Step 2 presque bouclée. Il manque le multi retour et un problème lorsqu'on ajoute un utilisateur avec certains soucis

--- Etape_2/quest-spring-boot/src/main/java/com/quest/etna/controller/AuthenticationController.java
+++ Etape_2/quest-spring-boot/src/main/java/com/quest/etna/controller/AuthenticationController.java
@@ -1,18 +1,33 @@
 package com.quest.etna.controller;
 
+import com.quest.etna.model.User;
+import com.quest.etna.model.UserDetails;
 import com.quest.etna.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AuthenticationController {
 
-    private static UserRepository _userRepository;
+    @Autowired
+    private UserRepository _userRepository;
 
     @PostMapping(value = "/register")
-    @ResponseStatus(code = HttpStatus.OK)
-    public String testSuccess(@RequestBody String username, @RequestBody String password) {
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public ResponseEntity<UserDetails> register(@RequestBody User user) {
 
-        return "success" + username + " / " + password;
+        User foundedUser = _userRepository.getUserByUsername(user.getUsername());
+        if (null != foundedUser)
+            return new ResponseEntity("Already exist", HttpStatus.CONFLICT);
+
+        try {
+            user = _userRepository.save(user);
+            UserDetails userDetails = new UserDetails(user.getUsername(), user.getRole());
+            return new ResponseEntity(userDetails, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- Etape_2/quest-spring-boot/src/main/java/com/quest/etna/model/UserDetails.java
+++ Etape_2/quest-spring-boot/src/main/java/com/quest/etna/model/UserDetails.java
@@ -4,4 +4,27 @@ public class UserDetails {
 
     private String username;
     private UserRole role;
+
+    public UserDetails() {}
+
+    public UserDetails(String username, UserRole role) {
+        this.username = username;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
 }
