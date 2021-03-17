383e05e60f55d49d4f5434e2a28d0b4314d3b420
alexandremajda <majda.a@outlook.com>
2021-03-17 23:15:34 +0100

    [Feature] Step G

--- Etape_3/quest-spring-boot/src/main/java/com/quest/etna/config/JwtAuthenticationEntryPoint.java
+++ Etape_3/quest-spring-boot/src/main/java/com/quest/etna/config/JwtAuthenticationEntryPoint.java
@@ -15,6 +15,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
 
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-        httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 }
