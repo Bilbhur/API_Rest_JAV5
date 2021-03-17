f598aadf615fb2ac46afc86d9e78b13c99f7fb05
alexandremajda <majda.a@outlook.com>
2021-03-17 23:13:06 +0100

    [Feature] Implement JWT : beginning

--- Etape_3/quest-spring-boot/src/main/java/com/quest/etna/config/JwtRequestFilter.java
+++ Etape_3/quest-spring-boot/src/main/java/com/quest/etna/config/JwtRequestFilter.java
@@ -1,4 +1,43 @@
 package com.quest.etna.config;
 
-public class JwtRequestFilter {
+import com.quest.etna.model.User;
+import com.quest.etna.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String authTokenHeader = httpServletRequest.getHeader("Authorization");
+        String username = jwtTokenUtil.getUsernameFromToken(authTokenHeader);
+        User user = userRepository.findByUsername(username);
+
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        SecurityContext sc = SecurityContextHolder.getContext();
+        sc.setAuthentication(auth);
+    }
 }
