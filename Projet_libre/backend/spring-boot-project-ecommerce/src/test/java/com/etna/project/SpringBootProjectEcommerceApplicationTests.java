22a829ce572e708c5e4907991410fef3cf26fdc1
boris <lepage_b@etna-alternance.net>
2021-04-09 18:31:43 +0200

    Start Backend

---
+++ Projet_libre/backend/spring-boot-project-ecommerce/src/test/java/com/etna/project/SpringBootProjectEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etna.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
